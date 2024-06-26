8c169c6f7bef67353f5ce5dbba19a1bcd116d193
Боян Панчев <panchev68@gmail.com>
2024-06-26 10:41:01 +0300

    Update TouchGFX_retarget.cpp

--- PROJECT/BSP/TouchGFX_retarget.cpp
+++ PROJECT/BSP/TouchGFX_retarget.cpp
@@ -1,5 +1,19 @@
 #include "PROJECT/BSP.inl"
 
+///  \brief Display Driver Transmit Block
+/// \details transmit memory buffer to display controller
+/// \param[in] src - Pointer to source buffer
+/// \param[in] x - X position
+/// \param[in] y - Y position
+/// \param[in] w - Width of window
+/// \param[in] h - Height of window
+/// \return size of transmitted block
+/// \ingroup TouchGFX
+extern "C" void touchgfxDisplayDriverTransmitBlock (const uint8_t * src, uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+    BSP::tft.transmitBlock (reinterpret_cast<const uint16_t *> (src), x, y, w, h);
+}
+
 /// \brief Display Driver Transmit Active
 /// \ingroup TouchGFX
 extern "C" int touchgfxDisplayDriverTransmitActive ()
@@ -14,18 +28,4 @@ extern "C" int touchgfxDisplayDriverTransmitActive ()
 extern "C" int touchgfxDisplayDriverShouldTransferBlock (uint16_t bottom)
 {
     return 1;
-}
-
-///  \brief Display Driver Transmit Block
-/// \details Copy Buffer to Display
-/// \param[in] src - pointer to source buffer
-/// \param[in] x - X Position
-/// \param[in] y - Y Position
-/// \param[in] width - Width of Window
-/// \param[in] height - Height of Window
-/// \return size of transmitted block
-/// \ingroup TouchGFX
-extern "C" void touchgfxDisplayDriverTransmitBlock (const uint8_t * src, uint16_t x, uint16_t y, uint16_t w, uint16_t h)
-{
-    BSP::tft.transmitBlock (reinterpret_cast<const uint16_t *> (src), x, y, w, h);
 }
